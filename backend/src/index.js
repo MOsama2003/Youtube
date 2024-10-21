52c734694bc15a02313d9dc90c65b42126618206
MOsama2003 <mo842825@gmail.com>
2024-10-22 01:37:26 +0500

    bb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,13 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config({path:'./.env'});
-connectDB()
+connectDB().then(()=>{
+    app.listen(8000,()=>(
+        console.log('Server is running on PORT 8000')
+    ))
+})
+.catch((err) => {
+    console.log(err)
+})
